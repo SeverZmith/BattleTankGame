7f63d6762caccf3b81ee321f2d5b66abe6ea0174
SeverZmith <eric.g.smith.24@gmail.com>
2018-05-19 23:51:12 -0700

    C_09 Added GetPlayerTank() to TankAIController class. Implemented Tick in TankPlayerController class.

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,5 +19,10 @@ public:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
 	
+	/* Move the tank's barrel towards the crosshair to shoot
+		where the crosshair is pointing */
+	void AimTowardsCrosshair();
 };
